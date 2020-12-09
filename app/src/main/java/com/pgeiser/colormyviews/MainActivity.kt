dfac19a77df96d540b33cd75d8ee8c98288a6267
13pgeiser <pgeiser@pgeiser.com>
2020-12-09 15:01:28 +0100

    Add the remaining buttons

--- app/src/main/java/com/pgeiser/colormyviews/MainActivity.kt
+++ app/src/main/java/com/pgeiser/colormyviews/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.pgeiser.colormyviews.databinding.ActivityMainBinding
 
@@ -18,7 +19,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun setListeners() {
         val allViews : List<View> = listOf(binding.boxOneText, binding.boxTwoText,
-            binding.boxThreeText, binding.boxFourText, binding.boxFiveText, binding.constraintLayout)
+            binding.boxThreeText, binding.boxFourText, binding.boxFiveText,
+            binding.redButton, binding.yellowButton, binding.greenButton,
+            binding.constraintLayout)
         for (item in allViews) {
             item.setOnClickListener {  makeColored(it) }
         }
@@ -35,6 +38,10 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            R.id.red_button -> binding.boxThreeText.setBackgroundColor(ContextCompat.getColor(this, (R.color.my_red)))
+            R.id.yellow_button -> binding.boxFourText.setBackgroundColor(ContextCompat.getColor(this, (R.color.my_yellow)))
+            R.id.green_button -> binding.boxFiveText.setBackgroundColor(ContextCompat.getColor(this, (R.color.my_green)))
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
